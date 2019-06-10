673e646890683e3787a8308fd24c4eb7eefe54c6
Dan Horwood <dpj@sent.com>
2019-06-10 11:56:51 +0800

    part 1 exercise 1.6 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,50 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const FeedbackButtons = ({props}) => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={props[0].handleClick}>
+        {props[0].name}
+      </button>
+      <button onClick={props[1].handleClick}>
+        {props[1].name}
+      </button>
+      <button onClick={props[2].handleClick}>
+        {props[2].name}
+      </button>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const buttons = [
+    {
+      name: "good",
+      handleClick: () => setGood(good + 1)
+    },
+    {
+      name: "neutral",
+      handleClick: () => setNeutral(neutral + 1)
+    },
+    {
+      name: "bad",
+      handleClick: () => setBad(bad + 1)
+    }
+  ]
   return (
     <div>
-      code here
+      <FeedbackButtons props={buttons} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
