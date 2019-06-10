40e9098a7f5fc5ec7f7bdb9627c807a58122fc4a
Dan Horwood <dpj@sent.com>
2019-06-10 09:25:55 +0800

    part 1 exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,35 +3,37 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
   return (
-    <>
-    <h1>{props.course}</h1>
-    </>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <>
+        <p>{props.part} {props.exercises}</p>
+      </>
     )
   }
   
   const Content = (props) => {
     return (
-      <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-      </>
+      <div>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
+      </div>
       )
     }
     
     const Total = (props) => {
       return (
-        <>
-        <p>
-          Number of exercises {props.total}
-        </p>
-        </>
+        <div>
+          <p>
+            Number of exercises {props.total}
+          </p>
+        </div>
         )
       }
       
