6a12cf6ed58d278ba98f28b696caac095bdc5c8a
Dan Horwood <dpj@sent.com>
2019-06-10 12:29:41 +0800

    part 1 exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const FeedbackButtons = ({props}) => {
+const FeedbackButtons = ({buttons}) => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={props[0].handleClick}>
-        {props[0].name}
+      <button onClick={[0].handleClick}>
+        {buttons[0].name}
       </button>
-      <button onClick={props[1].handleClick}>
-        {props[1].name}
+      <button onClick={buttons[1].handleClick}>
+        {buttons[1].name}
       </button>
-      <button onClick={props[2].handleClick}>
-        {props[2].name}
+      <button onClick={buttons[2].handleClick}>
+        {buttons[2].name}
       </button>
     </div>
   )
 }
+
+
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let totalClicks = good + neutral + bad
+  let average = (good - bad) / totalClicks
+  let positivePercentage = good / totalClicks * 100
 
   const buttons = [
     {
@@ -40,11 +46,14 @@ const App = () => {
   ]
   return (
     <div>
-      <FeedbackButtons props={buttons} />
+      <FeedbackButtons buttons={buttons} />
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalClicks}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage} %</p>
     </div>
   )
 }
