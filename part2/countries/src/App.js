1d911ce5d102fb272a9aab574bf23424fdbb2510
Dan Horwood <dpj@sent.com>
2019-06-11 16:09:06 +0800

    part 2 exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,18 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
-function Countries({countries}) {
-  if (countries.length > 10) {
-    return <p>Too many matches, specify another filter</p>
-  }
-  else if (countries.length > 1) {
-    return countries.map(c => <p key={c.name}>{c.name}</p>)
-  }
-  else if (countries.length === 0) {
-    return <p>Nothing to show</p>
-  }
-
-  const country = countries[0]
+function Country({country}) {
   return (
     <div>
       <h2>{country.name}</h2>
@@ -26,9 +15,40 @@ function Countries({countries}) {
   )
 }
 
+function CountryList({countries, buttonHandler}) {
+  return countries.map(c =>
+    <p key={c.name}>{c.name} <button onClick={buttonHandler(c)}>show</button></p>)
+}
+
+function Countries({countries, country, buttonHandler}) {
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+  else if (countries.length === 0) {
+    return <p>Nothing to show</p>
+  }
+
+  if (countries.length === 1) country = countries[0]
+  console.log(country)
+
+  return (
+    <div>
+      {countries.length > 1 &&
+        <CountryList countries={countries} buttonHandler={buttonHandler} />
+      }
+      {country !== null &&
+        <Country country={country ? country : countries[0]} />
+      }
+    </div>
+  )
+}
+
+
 function App() {
   const [countries, setCountries] = useState([])
   const [filterString, setFilterString] = useState("")
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
   const countriesToShow =
     countries.filter(c => c.name.toUpperCase().includes(filterString.toUpperCase()))
 
@@ -44,10 +64,14 @@ function App() {
     setFilterString(event.target.value)
   }
 
+  const handleButtonPress = (country) => () => {
+    setSelectedCountry(country)
+  }
+
   return (
     <div>
       <p>find {countriesToShow.length} countries <input value={filterString} onChange={handleFilterChange}/></p>
-      <Countries countries={countriesToShow} />
+      <Countries countries={countriesToShow} country={selectedCountry} buttonHandler={handleButtonPress} />
     </div>
   );
 }
