8fe2a07e835a47244d639ad82d8ff14940effe97
Dan Horwood <dpj@sent.com>
2019-06-13 10:38:53 +0800

    part 2 exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -30,9 +30,14 @@ const App = () => {
     }
     else {
       const newPerson = {name: newName, number: newNumber}
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')    
+        })
     }
   }
 
